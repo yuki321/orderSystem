ffe86e726b8411f4bea718456e8c2447bf4d90db
yuki321 <yukiikuy@live.jp>
2022-05-15 23:56:22 +0900

    前回の追加修正 インターフェイスの不要メソッドの削除

--- app/Interfaces/OrderHistoriesRepositoryInterface.php
+++ app/Interfaces/OrderHistoriesRepositoryInterface.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 interface OrderHistoriesRepositoryInterface {
 
     public function getAllOrderHistories(Request $request);
-    public function getItemName(Request $request);
-    public function getCompanyName(Request $request);
 
 }
 
