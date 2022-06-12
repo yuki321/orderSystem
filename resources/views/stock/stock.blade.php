cf30ed805395e31b25db71e9e6277b4fa62d571d
yuki321 <yukiikuy@live.jp>
2022-06-12 11:02:34 +0900

    tailwindcss関連の設定を変更 ※逐次コンパイルができない

--- resources/views/stock/stock.blade.php
+++ resources/views/stock/stock.blade.php
@@ -5,81 +5,83 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('/js/app.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="css/main.css">
-    <script src="https://cdn.tailwindcss.com"></script>
     <title>Stock</title>
 </head>
 <body>
-    <a href={{ route('logout') }} onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-        <b>Logout</b>
-    </a>
-    <form id='logout-form' action={{ route('logout')}} method="POST" style="display: none;">
-        @csrf
-    </form>
+    <div class="m-6">
+        <a href={{ route('logout') }} onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            <b>Logout</b>
+        </a>
+        <form id='logout-form' action={{ route('logout')}} method="POST" style="display: none;">
+            @csrf
+        </form>
 
-    <h1>在庫一覧画面</h1>
-    <div>
-        <a href="/dashboard">Main</a><br>
-    </div>
+        <h1>在庫一覧画面</h1>
+        <div>
+            <a href="/dashboard" class="text-gray-500 underline-offset-auto">Main</a><br>
+        </div>
+        
+        <form action="/content" method="get">
+            @csrf
+            <p>
+                検索ワード: <input type="text" name="search">
+            </p>
 
-    <form action="/content" method="get">
-        @csrf
-        <p>
-            検索ワード: <input type="text" name="search">
-        </p>
+            <p>
+                実在庫数(上限): <input type="number" name="max">
+            </p>
+            <p>
+                実在庫数(下限): <input type="number" name="min">
+            </p>
+            <input type="submit" value="検索" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  my-4 rounded">
+        </form>
 
-        <p>
-            実在庫数(上限): <input type="number" name="max">
-        </p>
-        <p>
-            実在庫数(下限): <input type="number" name="min">
-        </p>
-        <input type="submit" value="検索">
-    </form>
-
-    @if($contents->count() > 0)
-    <table class="table">
-        <thead>
-            <th>#</th>
-            <th scope="col">商品名</th>
-            <th scope="col">実在庫数</th>
-            <th scope="col">下限在庫数</th>
-            <th scope="col">週間減少在庫数</th>
-        </thead>
-        <tbody>
-            @foreach($contents as $content)
-            <tr>
-                <td scope="row">{{ $content->id }}</td>
-                <td><a href="/stockLowerLimit/{{ $content->id }}">{{ $content->itemName }}</a></td>
-                <td>{{ $content->actualStock }}</td>
-                <td>{{ $content->minStock }}</td>
-                <td>{{ $content->decreasePerWeek }}</td>
-                @if($content->actualStock < $content->minStock)
-                
-                <!-- 管理者の場合、商品の下限在庫数設定機能を使用することができる -->
+        @if($contents->count() > 0)
+        <table class="table">
+            <thead>
+                <th>#</th>
+                <th scope="col">商品名</th>
+                <th scope="col">実在庫数</th>
+                <th scope="col">下限在庫数</th>
+                <th scope="col">週間減少在庫数</th>
+            </thead>
+            <tbody>
+                @foreach($contents as $content)
+                <tr>
+                    <td scope="row">{{ $content->id }}</td>
+                    <td><a href="/stockLowerLimit/{{ $content->id }}" class="underline decoration-sky-600 hover:bg-blue-400">{{ $content->itemName }}</a></td>
+                    <td>{{ $content->actualStock }}</td>
+                    <td>{{ $content->minStock }}</td>
+                    <td>{{ $content->decreasePerWeek }}</td>
+                    @if($content->actualStock < $content->minStock)
+                    
+                    <!-- 管理者の場合、商品の下限在庫数設定機能を使用することができる -->
                     @if($isAdmin)
                     <td style="color: red;"><a href="/stockLowerLimit/{{ $content->id }}"><b>在庫が下限在庫数を下回っています。</b></a></td>
                     @else
-                    <td style="color: red;"><b>在庫が下限在庫数を下回っています。</b></td>
-                    @endif
-                @elseif($content->actualStock - $content->decreasePerWeek < $content->minStock)
-                    @if($isAdmin)
-                    <td style="color: skyblue"><a href="/stockLowerLimit/{{ $content->id }}">1週間以内に在庫が下限在庫数を下回る可能性が高いです。</a></td>
-                    @else
-                    <td style="color: skyblue">1週間以内に在庫が下限在庫数を下回る可能性が高いです。</td>
-                    @endif
-                @endif
-            </tr>
-            @endforeach
-        </tbody> 
-    </table>
-    @else
-    <p><b>該当するデータはありません。</b></p>
-    @endif
-
-    {{ $contents->links() }}
-
+                        <td style="color: red;"><b>在庫が下限在庫数を下回っています。</b></td>
+                        @endif
+                        @elseif($content->actualStock - $content->decreasePerWeek < $content->minStock)
+                        @if($isAdmin)
+                        <td style="color: skyblue"><a href="/stockLowerLimit/{{ $content->id }}">1週間以内に在庫が下限在庫数を下回る可能性が高いです。</a></td>
+                        @else
+                        <td style="color: skyblue">1週間以内に在庫が下限在庫数を下回る可能性が高いです。</td>
+                        @endif
+                        @endif
+                </tr>
+                @endforeach
+            </tbody> 
+        </table>
+        @else
+        <p><b>該当するデータはありません。</b></p>
+        @endif
+        
+        {{ $contents->links() }}
+    </div>
+    
 </body>
 </html>
 
